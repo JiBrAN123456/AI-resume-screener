6f11d8e56eba6cd191a2a605ba943ab3de9ddbae
Mohammad Jibran <yunisquadri99@gmail.com>
2025-03-23 02:17:33 +0530

    better

--- resumes/models.py
+++ resumes/models.py
@@ -9,16 +9,16 @@ User = get_user_model()
 
 class Resume(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4 , editable= False)
-    user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="resumes")
+    user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="resumes/")
     file = models.FileField(upload_to='resumes/')
     extracted_text = models.TextField(blank=True , null= True)
     name = models.CharField(max_length=255, blank=True , null =True)
     email = models.EmailField(blank=True , null = True)
-    phone = models.IntegerField(blank=True , null = True)
+    phone = models.CharField(max_length=200 ,blank=True , null = True)
     skills = models.TextField(null= True , blank= True)
     education = models.TextField(null= True , blank= True)
     experience = models.TextField(null= True , blank= True)
-    uplaoded_at =  models.DateTimeField(auto_now_add=True)
+    uploaded_at =  models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.email} - {self.name or "Resume"}"
